588cbfc8ae05f9232d37da6ac19bb1cad437deef
Gabriel Enrique Angulo Gonzalez <gaboangulo1@gmail.com>
2026-07-22 17:22:22 -0400

    I'm cooking hold on

--- auto-update.sh
+++ auto-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
-sudo apt-get update -qq
+apt-get update -qq
 UPDATES=$(sudo /usr/lib/update-notifier/apt-check 2>&1 | cut -d';' -f1)
 if [[ "$UPDATES" -gt 0 ]]; then
-    sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y
+    apt-get dist-upgrade -y && apt-get autoremove -y
 fi
